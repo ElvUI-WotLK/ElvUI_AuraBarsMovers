e04466cb0668d9d8667c322ee9c4647b17d6483f
bunny67 <zolov6767@gmail.com>
2019-08-20 18:38:40 +0300

    bump version

--- ElvUI_AuraBarsMovers/core.lua
+++ ElvUI_AuraBarsMovers/core.lua
@@ -1,227 +1,127 @@
-local E, L, V, P, G = unpack(ElvUI)
-local ABM = E:NewModule("AuraBarMovers", "AceHook-3.0", "AceEvent-3.0")
-local UF = E:GetModule("UnitFrames")
+local E, L, V, P, G = unpack(ElvUI);
+local ABM = E:NewModule('AuraMover', 'AceHook-3.0', 'AceEvent-3.0');
+local UF = E:GetModule('UnitFrames');
+local targetInsert = false
+local focusInsert = false
 local EP = LibStub("LibElvUIPlugin-1.0")
 local addon = ...
 
-P["abm"] = {
-	["player"] = false,
-	["target"] = false,
-	["focus"] = false,
-	["playerw"] = E.db.unitframe.units.player.width,
-	["targetw"] = E.db.unitframe.units.target.width,
-	["focusw"] = E.db.unitframe.units.focus.width,
-	["playerSpace"] = 0,
-	["targetSpace"] = 0,
-	["focusSpace"] = 0
+--GLOBALS: hooksecurefunc, CreateFrame
+local _G = _G
+
+P['abm'] = {
+	['player'] = false,
+	['target'] = false,
+	['focus'] = false,
+	['pet'] = false,
+	['playerw'] = E.db.unitframe.units.player.width,
+	['targetw'] = E.db.unitframe.units.target.width,
+	['focusw'] = E.db.unitframe.units.focus.width,
+	['petw'] = E.db.unitframe.units.pet.width,
+	['playerSpace'] = 0,
+	['targetSpace'] = 0,
+	['focusSpace'] = 0,
+	['petSpace'] = 0,
 }
 
 function ABM:PlayerABmove()
-	local auraBar = ElvUF_Player.AuraBars
-
-	local holder = CreateFrame("Frame", nil, auraBar)
-	holder:Point("BOTTOM", ElvUF_Player, "TOP", 0, 0)
-	auraBar:Point("BOTTOM", holder, "TOP", 0, 0)
+	local auraBar = _G["ElvUF_Player"].AuraBars
+	--Create Holder frame for our AuraBar Mover
+	local holder = CreateFrame('Frame', nil, auraBar)
+	holder:Point("BOTTOM", _G["ElvUF_Player"], "TOP", 0, 0)
+	auraBar:SetPoint("BOTTOM", holder, "TOP", 0, 0)
 	auraBar.Holder = holder
 
-	E:CreateMover(auraBar.Holder, "ElvUF_PlayerAuraMover", L["Player Aura Bars"], nil, nil, nil, "ALL,SOLO")
-	UF:CreateAndUpdateUF("player")
+	E:CreateMover(auraBar.Holder, 'ElvUF_PlayerAuraMover',  "Player Aura Bars", nil, nil, nil, 'ALL,SOLO')
+	UF:CreateAndUpdateUF('player')
 end
 
 function ABM:TargetABmove()
-	local auraBar = ElvUF_Target.AuraBars
-
-	local holder = CreateFrame("Frame", nil, auraBar)
-	holder:Point("BOTTOM", ElvUF_Target, "TOP", 0, 0)
-	auraBar:Point("BOTTOM", holder, "TOP", 0, 0)
+	local auraBar = _G["ElvUF_Target"].AuraBars
+	--Create Holder frame for our AuraBar Mover
+	local holder = CreateFrame('Frame', nil, auraBar)
+	holder:Point("BOTTOM", _G["ElvUF_Target"], "TOP", 0, 0)
+	auraBar:SetPoint("BOTTOM", holder, "TOP", 0, 0)
 	auraBar.Holder = holder
 
-	E:CreateMover(auraBar.Holder, "ElvUF_TargetAuraMover", L["Target Aura Bars"], nil, nil, nil, "ALL,SOLO")
-	UF:CreateAndUpdateUF("target")
+	E:CreateMover(auraBar.Holder, 'ElvUF_TargetAuraMover',  "Target Aura Bars", nil, nil, nil, 'ALL,SOLO')
+	UF:CreateAndUpdateUF('target')
 end
 
 function ABM:FocusABmove()
-	local auraBar = ElvUF_Focus.AuraBars
-
-	local holder = CreateFrame("Frame", nil, auraBar)
-	holder:Point("BOTTOM", ElvUF_Focus, "TOP", 0, 0)
-	auraBar:Point("BOTTOM", holder, "TOP", 0, 0)
+	local auraBar = _G["ElvUF_Focus"].AuraBars
+	--Create Holder frame for our AuraBar Mover
+	local holder = CreateFrame('Frame', nil, auraBar)
+	holder:Point("BOTTOM", _G["ElvUF_Focus"], "TOP", 0, 0)
+	auraBar:SetPoint("BOTTOM", holder, "TOP", 0, 0)
 	auraBar.Holder = holder
 
-	E:CreateMover(auraBar.Holder, "ElvUF_FocusAuraMover", L["Focus Aura Bars"], nil, nil, nil, "ALL,SOLO")
-	UF:CreateAndUpdateUF("focus")
+	E:CreateMover(auraBar.Holder, 'ElvUF_FocusAuraMover',  "Focus Aura Bars", nil, nil, nil, 'ALL,SOLO')
+	UF:CreateAndUpdateUF('focus')
 end
 
-function ABM:UpdatePlayer(frame, db)
-	frame.db = db
-	local POWERBAR_OFFSET = db.power.offset
-	if not db.aurabar.enable then return end
+function ABM:PetABmove()
+	local auraBar = _G["ElvUF_Pet"].AuraBars
+	--Create Holder frame for our AuraBar Mover
+	local holder = CreateFrame('Frame', nil, auraBar)
+	holder:Point("BOTTOM", _G["ElvUF_Pet"], "TOP", 0, 0)
+	auraBar:SetPoint("BOTTOM", holder, "TOP", 0, 0)
+	auraBar.Holder = holder
 
+	E:CreateMover(auraBar.Holder, 'ElvUF_PetAuraMover',  "Pet Aura Bars", nil, nil, nil, 'ALL,SOLO')
+	UF:CreateAndUpdateUF('pet')
+end
+
+function ABM:Configure_AuraBars(frame)
+	local unit = frame.unit
+	local db = frame.db
+	if not db.aurabar.enable then return end
+	if E.db.abm[unit] == nil then return end
+	local POWERBAR_OFFSET = db.power.offset
 	local auraBars = frame.AuraBars
 
 	if frame and auraBars and auraBars.spacing then
-		auraBars.spacing = (E.PixelMode and -1 or 1) + E.db.abm.playerSpace
+		auraBars.spacing = (E.PixelMode and -1 or 1) + E.db.abm[unit.."Space"]
 	end
 
-	local anchorPoint, anchorTo = "BOTTOM", "TOP"
-	if db.aurabar.anchorPoint == "BELOW" then
-		anchorPoint, anchorTo = "TOP", "BOTTOM"
+	local anchorPoint, anchorTo = 'BOTTOM', 'TOP'
+	if db.aurabar.anchorPoint == 'BELOW' then
+		anchorPoint, anchorTo = 'TOP', 'BOTTOM'
 	end
-
-	auraBars.Holder:Width(E.db.abm.playerw)
+	
+	--Set size of mover
+	auraBars.Holder:Width(E.db.abm[unit.."w"])
 	auraBars.Holder:Height(20)
-	auraBars.Holder:GetScript("OnSizeChanged")(auraBars.Holder)
+	auraBars.Holder:GetScript('OnSizeChanged')(auraBars.Holder)
 
 	auraBars:ClearAllPoints()
-
-	if not E.db.abm.player then
-		if db.aurabar.attachTo == "BUFFS" then
+	
+	if not E.db.abm[unit] then
+		local attachTo
+		if db.aurabar.attachTo == 'BUFFS' then
 			attachTo = frame.Buffs
-		elseif db.aurabar.attachTo == "DEBUFFS" then
+		elseif db.aurabar.attachTo == 'DEBUFFS' then
 			attachTo = frame.Debuffs
 		end
 
-		local yOffset = 0
+		local yOffset = 0;
 		if E.PixelMode then
-			if db.aurabar.anchorPoint == "BELOW" then
-				yOffset = 1
+			if db.aurabar.anchorPoint == 'BELOW' then
+				yOffset = 1;
 			else
-				yOffset = -1
+				yOffset = -1;
 			end
 		end
 
-		local BarWidth = E.db.abm.playerw - db.width
-		local xCoord = attachTo == frame and POWERBAR_OFFSET * (anchorTo == "BOTTOM" and 0 or -1) or 0
+		local BarWidth = E.db.abm[unit.."w"] - db.width
+		local xCoord = attachTo == frame and POWERBAR_OFFSET * (anchorTo == 'BOTTOM' and 0 or -1) or 0
 		xCoord = xCoord + BarWidth
 
-		auraBars:Point(anchorPoint.."LEFT", attachTo, anchorTo.."LEFT", (attachTo == frame and anchorTo == "BOTTOM") and POWERBAR_OFFSET or 0, E.PixelMode and anchorPoint ==  -1 or yOffset)
-		auraBars:Point(anchorPoint.."RIGHT", attachTo, anchorTo.."RIGHT", xCoord, E.PixelMode and -1 or yOffset)
-
-		E:DisableMover("ElvUF_PlayerAuraMover")
+		auraBars:SetPoint(anchorPoint..'LEFT', attachTo, anchorTo..'LEFT', (attachTo == frame and anchorTo == 'BOTTOM') and POWERBAR_OFFSET or 0, E.PixelMode and anchorPoint ==  -1 or yOffset)
+		auraBars:SetPoint(anchorPoint..'RIGHT', attachTo, anchorTo..'RIGHT', xCoord, E.PixelMode and -1 or yOffset)
 	else
-		E:EnableMover("ElvUF_PlayerAuraMover")
-
-		auraBars:Point(anchorPoint.."LEFT", auraBars.Holder, anchorTo.."LEFT")
-		auraBars:Point(anchorPoint.."RIGHT", auraBars.Holder, anchorTo.."RIGHT", -POWERBAR_OFFSET, 0)
-	end
-	auraBars:SetAnchors()
-
-	frame:UpdateAllElements("AurabarsMovers_UpdateAllElements")
-	if auraBars.bars[1] and auraBars.bars[1]:GetWidth() ~= E.db.abm.playerw then auraBars.bars[1]:SetWidth(E.db.abm.playerw - auraBars.bars[1].statusBar.icon:GetWidth()) end
-end
-
-function ABM:UpdateTarget(frame, db)	
-	frame.db = db
-	local POWERBAR_OFFSET = db.power.offset
-	if not db.aurabar.enable then return end
-
-	local auraBars = frame.AuraBars
-
-	if frame and auraBars and auraBars.spacing then
-		auraBars.spacing = (E.PixelMode and -1 or 1) + E.db.abm.targetSpace
-	end
-
-	local anchorPoint, anchorTo = "BOTTOM", "TOP"
-	if db.aurabar.anchorPoint == "BELOW" then
-		anchorPoint, anchorTo = "TOP", "BOTTOM"
-	end
-
-	auraBars.Holder:Width(E.db.abm.targetw)
-	auraBars.Holder:Height(20)
-	auraBars.Holder:GetScript("OnSizeChanged")(auraBars.Holder)
-
-	auraBars:ClearAllPoints()
-
-	if not E.db.abm.target then
-		local attachTo = frame
-
-		if db.aurabar.attachTo == "BUFFS" then
-			attachTo = frame.Buffs
-		elseif db.aurabar.attachTo == "DEBUFFS" then
-			attachTo = frame.Debuffs
-		elseif db.aurabar.attachTo == "PLAYER_AURABARS" and ElvUF_Player then
-			attachTo = ElvUF_Player.AuraBars
-		end
-
-		local yOffset = 0
-		if E.PixelMode then
-			if db.aurabar.anchorPoint == "BELOW" then
-				yOffset = 1
-			else
-				yOffset = -1
-			end
-		end
-
-		local BarWidth = E.db.abm.targetw - db.width
-		local xCoord = attachTo == frame and POWERBAR_OFFSET * (anchorTo == "BOTTOM" and 0 or -1) or 0
-		xCoord = xCoord + BarWidth
-
-		auraBars:Point(anchorPoint.."LEFT", attachTo, anchorTo.."LEFT", (attachTo == frame and anchorTo == "BOTTOM") and POWERBAR_OFFSET or 0, E.PixelMode and anchorPoint ==  -1 or yOffset)
-		auraBars:Point(anchorPoint.."RIGHT", attachTo, anchorTo.."RIGHT", xCoord, E.PixelMode and -1 or yOffset)
-
-		E:DisableMover("ElvUF_TargetAuraMover")
-	else
-		E:EnableMover("ElvUF_TargetAuraMover")
-
-		auraBars:Point(anchorPoint.."LEFT", auraBars.Holder, anchorTo.."LEFT")
-		auraBars:Point(anchorPoint.."RIGHT", auraBars.Holder, anchorTo.."RIGHT", -POWERBAR_OFFSET, 0)
-	end
-	auraBars:SetAnchors()
-
-	frame:UpdateAllElements("AurabarsMovers_UpdateAllElements")
-end
-
-function ABM:UpdateFocus(frame, db)	
-	frame.db = db
-	local POWERBAR_OFFSET = db.power.offset
-	if not db.aurabar.enable then return end
-
-	local auraBars = frame.AuraBars
-
-	if frame and auraBars and auraBars.spacing then
-		auraBars.spacing = (E.PixelMode and -1 or 1) + E.db.abm.focusSpace
-	end
-
-	local anchorPoint, anchorTo = "BOTTOM", "TOP"
-	if db.aurabar.anchorPoint == "BELOW" then
-		anchorPoint, anchorTo = "TOP", "BOTTOM"
-	end
-
-	auraBars.Holder:Width(E.db.abm.focusw)
-	auraBars.Holder:Height(20)
-	auraBars.Holder:GetScript("OnSizeChanged")(auraBars.Holder)
-
-	auraBars:ClearAllPoints()
-
-	if not E.db.abm.focus then
-		if db.aurabar.attachTo == "BUFFS" then
-			attachTo = frame.Buffs
-		elseif db.aurabar.attachTo == "DEBUFFS" then
-			attachTo = frame.Debuffs
-		end
-
-		local yOffset = 0
-		if E.PixelMode then
-			if db.aurabar.anchorPoint == "BELOW" then
-				yOffset = 1
-			else
-				yOffset = -1
-			end
-		end
-
-		local BarWidth = E.db.abm.focusw - db.width
-		local xCoord = attachTo == frame and POWERBAR_OFFSET * (anchorTo == "BOTTOM" and 0 or -1) or 0
-		xCoord = xCoord + BarWidth
-
-		auraBars:Point(anchorPoint.."LEFT", attachTo, anchorTo.."LEFT", (attachTo == frame and anchorTo == "BOTTOM") and POWERBAR_OFFSET or 0, E.PixelMode and anchorPoint ==  -1 or yOffset)
-		auraBars:Point(anchorPoint.."RIGHT", attachTo, anchorTo.."RIGHT", xCoord, E.PixelMode and -1 or yOffset)
-
-		E:DisableMover("ElvUF_FocusAuraMover")
-	else
-		E:EnableMover("ElvUF_FocusAuraMover")
-
-		auraBars:Point(anchorPoint.."LEFT", auraBars.Holder, anchorTo.."LEFT")
-		auraBars:Point(anchorPoint.."RIGHT", auraBars.Holder, anchorTo.."RIGHT", -POWERBAR_OFFSET, 0)
+		auraBars:SetPoint(anchorPoint..'LEFT', auraBars.Holder, anchorTo..'LEFT')
+		auraBars:SetPoint(anchorPoint..'RIGHT', auraBars.Holder, anchorTo..'RIGHT', -POWERBAR_OFFSET, 0)
 	end
 	auraBars:SetAnchors()
 
@@ -229,26 +129,44 @@ function ABM:UpdateFocus(frame, db)
 end
 
 function ABM:ChangeTarget()
-	UF:CreateAndUpdateUF("target")
+	UF:CreateAndUpdateUF('target')
 	self:UnregisterEvent("PLAYER_TARGET_CHANGED")
 end
 
-function ABM:Initialize()
-	EP:RegisterPlugin(addon, ABM.GetOptions)
+function ABM:MoverToggle()
+	if E.db.abm.player then
+		E:EnableMover("ElvUF_PlayerAuraMover")
+	else
+		E:DisableMover("ElvUF_PlayerAuraMover")
+	end
+	if E.db.abm.target then
+		E:EnableMover("ElvUF_TargetAuraMover")
+	else
+		E:DisableMover("ElvUF_TargetAuraMover")
+	end
+	if E.db.abm.focus then
+		E:EnableMover("ElvUF_FocusAuraMover")
+	else
+		E:DisableMover("ElvUF_FocusAuraMover")
+	end
+	if E.db.abm.pet then
+		E:EnableMover("ElvUF_PetAuraMover")
+	else
+		E:DisableMover("ElvUF_PetAuraMover")
+	end
+end
 
+function ABM:Initialize()
+	EP:RegisterPlugin(addon,ABM.GetOptions)
 	ABM:PlayerABmove()
 	ABM:TargetABmove()
 	ABM:FocusABmove()
+	ABM:PetABmove()
 
-	hooksecurefunc(UF, "Update_PlayerFrame", ABM.UpdatePlayer)
-	hooksecurefunc(UF, "Update_TargetFrame", ABM.UpdateTarget)
-	hooksecurefunc(UF, "Update_FocusFrame", ABM.UpdateFocus)
+	hooksecurefunc(UF, "Configure_AuraBars", ABM.Configure_AuraBars)
 
 	self:RegisterEvent("PLAYER_TARGET_CHANGED", "ChangeTarget")
+	ABM:MoverToggle()
 end
 
-local function InitializeCallback()
-	ABM:Initialize()
-end
-
-E:RegisterModule(ABM:GetName(), InitializeCallback)
+E:RegisterModule(ABM:GetName())
--- ElvUI_AuraBarsMovers/options.lua
+++ ElvUI_AuraBarsMovers/options.lua
@@ -1,115 +1,109 @@
-local E, L, V, P, G = unpack(ElvUI)
-local UF = E:GetModule("UnitFrames")
-local ABM = E:GetModule("AuraBarMovers")
-
-local function ColorizeSettingName(settingName)
-	return format("|cff1784d1%s|r", settingName)
-end
+local E, L, V, P, G = unpack(ElvUI);
+local UF = E:GetModule('UnitFrames');
+local ABM = E:GetModule('AuraMover')
 
 function ABM:GetOptions()
-	E.Options.args.auraBarMovers = {
-		order = 55,
-		type = "group",
-		name = ColorizeSettingName(L["Aura Bar Movers"]),
-		args = {
-			header = {
-				order = 1,
-				type = "header",
-				name = L["Aura Bar Movers"]
-			},
-			player = {
-				order = 2,
-				type = "group",
-				name = L["Player"],
-				guiInline = true,
-				args = {
-					detach = {
-						order = 1,
-						type = "toggle",
-						name = L["Detach From Frame"],
-						get = function(info) return E.db.abm.player end,
-						set = function(info, value) E.db.abm.player = value UF:CreateAndUpdateUF("player") end
-					},
-					width = {
-						order = 2,
-						type = "range",
-						name = L["Width"],
-						min = 50, max = 500, step = 1,
-						get = function(info) return E.db.abm.playerw end,
-						set = function(info, value) E.db.abm.playerw = value UF:CreateAndUpdateUF("player") end
-					},
-					space = {
-						order = 3,
-						type = "range",
-						name = L["Vertical Spacing"],
-						min = -10, max = 20, step = 1,
-						get = function(info) return E.db.abm.playerSpace end,
-						set = function(info, value) E.db.abm.playerSpace = value UF:CreateAndUpdateUF("player") end
-					}
-				}
-			},
-			target = {
-				order = 3,
-				type = "group",
-				name = L["Target"],
-				guiInline = true,
-				args = {
-					detach = {
-						order = 1,
-						type = "toggle",
-						name = L["Detach From Frame"],
-						get = function(info) return E.db.abm.target end,
-						set = function(info, value) E.db.abm.target = value UF:CreateAndUpdateUF("target") end
-					},
-					width = {
-						order = 2,
-						type = "range",
-						name = L["Width"],
-						min = 50, max = 500, step = 1,
-						get = function(info) return E.db.abm.targetw end,
-						set = function(info, value) E.db.abm.targetw = value UF:CreateAndUpdateUF("target") end
-					},
-					space = {
-						order = 3,
-						type = "range",
-						name = L["Vertical Spacing"],
-						min = -10, max = 20, step = 1,
-						get = function(info) return E.db.abm.targetSpace end,
-						set = function(info, value) E.db.abm.targetSpace = value UF:CreateAndUpdateUF("target") end
-					}
-				}
-			},
-			focus = {
-				order = 4,
-				type = "group",
-				name = L["Focus"],
-				guiInline = true,
-				args = {
-					detach = {
-						order = 1,
-						type = "toggle",
-						name = L["Detach From Frame"],
-						get = function(info) return E.db.abm.focus end,
-						set = function(info, value) E.db.abm.focus = value UF:CreateAndUpdateUF("focus") end
-					},
-					width = {
-						order = 2,
-						type = "range",
-						name = L["Width"],
-						min = 50, max = 500, step = 1,
-						get = function(info) return E.db.abm.focusw end,
-						set = function(info, value) E.db.abm.focusw = value UF:CreateAndUpdateUF("focus") end
-					},
-					space = {
-						order = 3,
-						type = "range",
-						name = L["Vertical Spacing"],
-						min = -10, max = 20, step = 1,
-						get = function(info) return E.db.abm.focusSpace end,
-						set = function(info, value) E.db.abm.focusSpace = value UF:CreateAndUpdateUF("focus") end
-					}
-				}
-			}
-		}
-	}
+E.Options.args.unitframe.args.player.args.aurabar.args.detach = {
+	order = 25,
+	type = "toggle",
+	name = '|cff30ee30'..L['Detach From Frame']..'|r',
+	get = function(info) return E.db.abm.player end,
+	set = function(info, value) E.db.abm.player = value; UF:CreateAndUpdateUF('player'); ABM:MoverToggle() end,
+}
+
+E.Options.args.unitframe.args.player.args.aurabar.args.width = {
+	order = 26,
+	type = "range",
+	name = '|cff30ee30'..L['Width']..'|r',
+	min = 50, max = 500, step = 1,
+	get = function(info) return E.db.abm.playerw end,
+	set = function(info, value) E.db.abm.playerw = value; UF:CreateAndUpdateUF('player') end,
+}
+
+E.Options.args.unitframe.args.player.args.aurabar.args.space = {
+	order = 27,
+	type = "range",
+	name = '|cff30ee30'..L["Vertical Spacing"]..'|r',
+	min = -10, max = 20, step = 1,
+	get = function(info) return E.db.abm.playerSpace end,
+	set = function(info, value) E.db.abm.playerSpace = value; UF:CreateAndUpdateUF('player') end,
+}
+
+E.Options.args.unitframe.args.target.args.aurabar.args.detach = {
+	order = 25,
+	type = "toggle",
+	name = '|cff30ee30'..L['Detach From Frame']..'|r',
+	get = function(info) return E.db.abm.target end,
+	set = function(info, value) E.db.abm.target = value; UF:CreateAndUpdateUF('target'); ABM:MoverToggle() end,
+}
+
+E.Options.args.unitframe.args.target.args.aurabar.args.width = {
+	order = 26,
+	type = "range",
+	name = '|cff30ee30'..L['Width']..'|r',
+	min = 50, max = 500, step = 1,
+	get = function(info) return E.db.abm.targetw end,
+	set = function(info, value) E.db.abm.targetw = value; UF:CreateAndUpdateUF('target') end,
+}
+
+E.Options.args.unitframe.args.target.args.aurabar.args.space = {
+	order = 27,
+	type = "range",
+	name = '|cff30ee30'..L["Vertical Spacing"]..'|r',
+	min = -10, max = 20, step = 1,
+	get = function(info) return E.db.abm.targetSpace end,
+	set = function(info, value) E.db.abm.targetSpace = value; UF:CreateAndUpdateUF('target') end,
+}
+
+E.Options.args.unitframe.args.focus.args.aurabar.args.detach = {
+	order = 25,
+	type = "toggle",
+	name = '|cff30ee30'..L['Detach From Frame']..'|r',
+	get = function(info) return E.db.abm.focus end,
+	set = function(info, value) E.db.abm.focus = value; UF:CreateAndUpdateUF('focus'); ABM:MoverToggle() end,
+}
+
+E.Options.args.unitframe.args.focus.args.aurabar.args.width = {
+	order = 26,
+	type = "range",
+	name = '|cff30ee30'..L['Width']..'|r',
+	min = 50, max = 500, step = 1,
+	get = function(info) return E.db.abm.focusw end,
+	set = function(info, value) E.db.abm.focusw = value; UF:CreateAndUpdateUF('focus') end,
+}
+
+E.Options.args.unitframe.args.focus.args.aurabar.args.space = {
+	order = 27,
+	type = "range",
+	name = '|cff30ee30'..L["Vertical Spacing"]..'|r',
+	min = -10, max = 20, step = 1,
+	get = function(info) return E.db.abm.focusSpace end,
+	set = function(info, value) E.db.abm.focusSpace = value; UF:CreateAndUpdateUF('focus') end,
+}
+
+E.Options.args.unitframe.args.pet.args.aurabar.args.detach = {
+	order = 25,
+	type = "toggle",
+	name = '|cff30ee30'..L['Detach From Frame']..'|r',
+	get = function(info) return E.db.abm.pet end,
+	set = function(info, value) E.db.abm.pet = value; UF:CreateAndUpdateUF('pet'); ABM:MoverToggle() end,
+}
+
+E.Options.args.unitframe.args.pet.args.aurabar.args.width = {
+	order = 26,
+	type = "range",
+	name = '|cff30ee30'..L['Width']..'|r',
+	min = 50, max = 500, step = 1,
+	get = function(info) return E.db.abm.petw end,
+	set = function(info, value) E.db.abm.petw = value; UF:CreateAndUpdateUF('pet') end,
+}
+
+E.Options.args.unitframe.args.pet.args.aurabar.args.space = {
+	order = 27,
+	type = "range",
+	name = '|cff30ee30'..L["Vertical Spacing"]..'|r',
+	min = -10, max = 20, step = 1,
+	get = function(info) return E.db.abm.petSpace end,
+	set = function(info, value) E.db.abm.petSpace = value; UF:CreateAndUpdateUF('pet') end,
+}
 end
